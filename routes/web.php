4dfd37400b39d0c616bbc328d0180eaa5aec112a
quyencao <boy20195@gmail.com>
2017-08-11 23:51:51 +0700

    Test 2 methods add_friend and accept_friend

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function () {
-    return Auth::user()->hello();
+Route::get('/add', function () {
+    return \App\User::first()->add_friend(2);
+});
+
+Route::get('accept', function () {
+    return App\User::find(2)->accept_friend(1);
 });
 
 Auth::routes();
