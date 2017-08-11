95fe18da91a53822fa06a221769e4ed054917acf
quyencao <boy20195@gmail.com>
2017-08-11 23:11:11 +0700

    Method for add_friend and accept_friend

--- app/Traits/Friendable.php
+++ app/Traits/Friendable.php
@@ -1,10 +1,39 @@
 <?php
     namespace App\Traits;
 
+    use App\Friendships;
+
     trait Friendable {
 
-        public function hello() {
-            return 'Hello friends';
+        public function add_friend($user_requested_id) {
+            $friendShips = Friendships::create([
+               'requester' => $this->id,
+               'user_requested' => $user_requested_id
+            ]);
+
+            if($friendShips) {
+                return response()->json($friendShips,200);
+            }
+
+            return response()->json('fails', 501);
+
         }
 
+        public function accept_friend($requester) {
+
+            $friendsShip = Friendships::where('requester', $requester)
+                                    ->where('user_requested', $this->id)
+                                    ->first();
+
+            if($friendsShip) {
+                $friendsShip->update([
+                   'status' => 1
+                ]);
+
+                return response()->json('ok', 200);
+            } else {
+                return response()->json('fail', 501);
+            }
+
+        }
     }
