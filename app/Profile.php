56f3b750c98424e02e03d75f9311d339a9c47485
quyencao <boy20195@gmail.com>
2017-08-11 16:55:42 +0700

    Create profile when create user

--- app/Profile.php
+++ app/Profile.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
-    protected $fillable = ['location', 'about'];
+    protected $fillable = ['location', 'about', 'user_id'];
 
     public function user() {
         return $this->belongsTo('App\User');
